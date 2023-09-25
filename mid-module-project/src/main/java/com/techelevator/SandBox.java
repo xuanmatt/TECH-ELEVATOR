c05ca9f932e5ebb71ca2f25f7056a3b432e94238
xuanmatt <98654820+xuanmatt@users.noreply.github.com>
2023-09-25 15:06:41 -0400

    Add files via upload

---
+++ mid-module-project/src/main/java/com/techelevator/SandBox.java
@@ -0,0 +1,37 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SandBox {
+
+    public static void main(String[] args) {
+
+
+        public List<Integer> findMostRecentBooks() {
+            List<Integer> mostRecentBooks = new ArrayList<>();
+        /*
+        Integer mostRecentYear = 0;
+        //Integer mostBookPublished = 0;
+
+        for (int i=0; i < publishedYears.size();i++){
+            if (publishedYears.get(i) > mostRecentYear){
+                mostRecentYear = publishedYears.get(i);
+            }
+        }
+        for (int i = 0; i < publishedYears.size(); i++){
+            if (mostRecentYear == publishedYears.get(i)){
+                mostRecentBooks.add(i);
+            }
+        }
+
+         */
+            List<Integer> publishedYears = new ArrayList<>();
+            int mostRecentBook = publishedYears.get(0);
+
+            mostRecentBooks.add(0); // I am confused of this code;
+            System.out.println(mostRecentBook);
+        }
+    }
+}
+
